046bd8553e7971547cc8827b496d4da6d0e5a59a
bigbadfred60 <84123818+bigbadfred60@users.noreply.github.com>
2021-05-13 11:23:14 +0200

    Added message code2

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-message = ["One","Two","Three"]
+message = ["One","Two","Three","Four"]
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
